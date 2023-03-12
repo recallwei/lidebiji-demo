aeb057f24ad1add4deeeab87973732d0b0794f22
Bruce <recall4056@gmail.com>
2023-03-12 23:13:30 +0800

    feat: add animation

--- src/components/ImageLoop/index.js
+++ src/components/ImageLoop/index.js
@@ -1,10 +1,23 @@
-import React from "react"
+import React, { useState, useRef, useLayoutEffect } from "react"
 import clsx from "clsx"
 import * as styles from "./index.module.css"
 
 const ImageLoop = ({ list }) => {
+  const [width, setWidth] = useState(0)
+
+  const ref = useRef(null)
+
+  useLayoutEffect(() => {
+    setWidth(ref.current.offsetWidth)
+    console.log(ref.current.offsetWidth)
+  }, [])
   return (
-    <div className={clsx("flex relative items-center gap-4 w-screen", styles.list)}>
+    <div
+      ref={ref}
+      key={width}
+      className={clsx("flex relative items-center gap-4", styles.list)}
+      style={{ "--offsetWidth": width }}
+    >
       {list.concat(list).map((item, index) => (
         <img
           key={index}
