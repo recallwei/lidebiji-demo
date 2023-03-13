ee05618a1b7bda03775e0a8e4f3d898b19de02bf
Bruce <recall4056@gmail.com>
2023-03-13 21:54:53 +0800

    chore: remove plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,29 +13,6 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-root-import",
     {
-      resolve: "gatsby-plugin-prettier-eslint",
-      options: {
-        prettier: {
-          patterns: [
-            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
-            "**/*.{css,scss,less}",
-            "**/*.{json,json5}",
-            "**/*.{graphql}",
-            "**/*.{md,mdx}",
-            "**/*.{html}",
-            "**/*.{yaml,yml}"
-          ]
-        },
-        eslint: {
-          patterns: "**/*.{js,jsx,ts,tsx}",
-          customOptions: {
-            fix: true,
-            cache: true
-          }
-        }
-      }
-    },
-    {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "static/favicon.png"
