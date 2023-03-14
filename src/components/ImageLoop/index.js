1c7d2ee1ffcb849feda886e21b48126fb6539343
Bruce <recall4056@gmail.com>
2023-03-14 10:06:51 +0800

    chore: adjust config

--- src/components/ImageLoop/index.js
+++ src/components/ImageLoop/index.js
@@ -3,6 +3,7 @@ import clsx from "clsx"
 import * as styles from "./index.module.css"
 
 const ImageLoop = ({ list, reverse, className }) => {
+  const lists = list.concat(list).concat(list)
   return (
     <div
       className={clsx(
@@ -12,18 +13,15 @@ const ImageLoop = ({ list, reverse, className }) => {
         className
       )}
     >
-      {list
-        .concat(list)
-        .concat(list)
-        .map((item, index) => (
-          <img
-            className="bg-white shadow-lg p-4 rounded-sm"
-            key={index}
-            src={item}
-            alt=""
-            width={200}
-          />
-        ))}
+      {lists.map((item, index) => (
+        <img
+          className="bg-white shadow-lg p-4 rounded-sm"
+          key={index}
+          src={item}
+          alt=""
+          width={200}
+        />
+      ))}
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
