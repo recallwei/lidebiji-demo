c07e90a37a39cfe218fd5441189488551b237d62
Bruce <recall4056@gmail.com>
2023-03-15 01:42:27 +0800

    fix: update nprogress color

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-nprogress",
       options: {
-        color: "blue",
+        color: "#2878FC",
         showSpinner: false
       }
     }
